240a768bd87b8fee0e1736c4b2276ceb3b83660e
saplyn <saplynx@outlook.com>
2024-06-10 23:33:47 +0800

    setup tailwind & primevue

--- app.vue
+++ app.vue
@@ -1,5 +1,9 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <Toast />
+
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  modules: ["nuxt-primevue", "@nuxtjs/tailwindcss", "@vueuse/nuxt"],
+  primevue: {
+    usePrimeVue: true,
+    options: {
+      unstyled: false,
+      ripple: true,
+    },
+    cssLayerOrder: "tailwind-base, primevue, tailwind-utilities",
+  },
+  css: [
+    "assets/css/tailwind.css",
+    "primevue/resources/themes/lara-light-blue/theme.css",
+    "primeicons/primeicons.css",
+  ],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  imports: {
+    dirs: ["types"],
+  },
+});
